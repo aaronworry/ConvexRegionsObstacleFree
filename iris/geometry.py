abe792baab88124ed15b0d48df5df436fe0433d9
aaronworry <aaronworry128@gmail.com>
2022-02-22 16:50:54 +0800

    iris-init

---
+++ iris/geometry.py
@@ -0,0 +1,15 @@
+
+
+class Ellipsoid():
+    def __init__(self):
+        pass
+
+class Hyperplane():
+    def __init__(self):
+        pass
+
+class Polyhedron():
+    def __init__(self):
+        pass
+
+
---
+++ iris/iris.py
@@ -0,0 +1,20 @@
+
+class IRISOptions():
+    def __init__(self):
+        pass
+
+class IRISRegion():
+    def __init__(self):
+        pass
+
+class IRISProblem():
+    def __init__(self):
+        pass
+
+
+def inner_ellipsoid():
+    pass
+
+def inflate_region():
+    pass
+
