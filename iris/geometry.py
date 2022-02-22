5f6446f3a0ac4bc52e66f03326e58761084c811f
aaronworry <aaronworry128@gmail.com>
2022-02-22 22:00:28 +0800

    iris-init, iris.cpp not finished

--- iris/geometry.py
+++ iris/geometry.py
@@ -1,15 +1,91 @@
+import numpy as np
+from iris import getGenerators
+import math
+
+ELLIPSOID_C_EPSILON = 1e-4
+
+def factorial(n):
+    return 1 if n == 0 else factorial(n - 1) * n
+
+def nSphereVolume(dim, radius):
+    k = dim // 2
+    if dim % 2 == 0:
+        v = (math.pi ** k) / factorial(k)
+    else:
+        v = 2 * factorial(k) * ((4 * math.pi) ** k) / factorial(2 * k + 1)
+    return v * (radius ** dim)
 
 
 class Ellipsoid():
-    def __init__(self):
-        pass
+    def __init__(self, dim, C, d):
+        self.C_ = C
+        self.dim = dim
+        self.d_ = d
+
+    def setCEntry(self, row, col, value):
+        self.C_[row][col] = value
+
+    def setDEntry(self, idx, value):
+        self.d_[idx] = value
+
+    def fromNSphere(self, center, radius):
+        dim = len(center)
+        C = radius * np.eye(dim)
+        return Ellipsoid(dim, C, center)
+
+    def getVolume(self):
+        return np.linalg.det(self.C_) * nSphereVolume(self.dim, 1.0)
+
+
 
 class Hyperplane():
-    def __init__(self):
-        pass
+    def __init__(self, dim, point, n):
+        """
+        initial a hyperplane
+        :param point: a point on the boundary of hyperplane
+        :param n: a normal vector point to the outside of hyperplane
+        """
+        self.dim = dim
+        self.point = point
+        self.n = n
 
 class Polyhedron():
-    def __init__(self):
-        pass
+    def __init__(self, dim, A, b):
+        self.dim = dim
+        self.A_ = A
+        self.b_ = b
+        self.dd_representation_dirty_ = True
+        self.generator_points_ = None
+        self.generator_rays_ = None
+
+    def updateDDRepresentation(self):
+        self.generator_rays_ = None
+        self.generator_points_ = None
+        getGenerators(self.A_, self.b_, self.generator_points_, self.generator_rays_)
+        self.dd_representation_dirty_ = False
+
+    def getNumberOfConstraints(self):
+        return len(self.b_)
+
+    def appendConstraints(self, otherPolyhedron):
+        self.A_ = np.vstack((self.A_, otherPolyhedron.A_))
+        self.b_ = np.vstack((self.b_, otherPolyhedron.b_))
+        self.dd_representation_dirty_ = True
+
+    def generatorPoints(self):
+        if self.dd_representation_dirty_:
+            self.updateDDRepresentation()
+        return self.generator_points_
+
+    def generatorRays(self):
+        if self.dd_representation_dirty_:
+            self.updateDDRepresentation()
+        return self.generator_rays_
+
+    def contains(self, point, tolerance):
+        return max(self.A_.dot(point) - self.b_) <= tolerance
 
 
+
+if __name__ == '__main__':
+    print(factorial(5))
--- iris/iris.py
+++ iris/iris.py
@@ -1,20 +1,78 @@
+import numpy as np
+from geometry import Ellipsoid, Polyhedron, Hyperplane
 
 class IRISOptions():
-    def __init__(self):
-        pass
+    def __init__(self, require_containment=False, required_containment_points=None, error_on_infeasible_start=False, termination_threshold=2e-2, iter_limit=100):
+        self.require_containment = require_containment
+        self.error_on_infeasible_start = error_on_infeasible_start
+        self.termination_threshold = termination_threshold
+        self.iter_limit = iter_limit
+        self.required_containment_points = required_containment_points
 
 class IRISRegion():
+    def __init__(self, dim):
+        self.dim = dim
+        self.polyhedron = Polyhedron(self.dim, None, None)
+        self.ellipsoid = Ellipsoid(self.dim, None, None)
+
+class IRISDebugData():
     def __init__(self):
-        pass
+        self.ellipsoid_history = []
+        self.polyhedron_history = []
+        self.obstacles = None
+        self.bounds = None
+        self.iters = None
+
+    def boundingPoints(self):
+        return self.bounds.generatorPoints()
 
 class IRISProblem():
-    def __init__(self):
-        pass
+    def __init__(self, dim):
+        self.obstacle_pts = []
+        self.bounds = None
+        self.dim = dim
+        self.seed = None
+
+    def setSeedPoint(self, point):
+        self.seed = Ellipsoid.fromNSphere(point, 0.01)
+
+    def setSeedEllipsoid(self, ellipsoid):
+        self.seed = ellipsoid
+
+    def setBounds(self, new_bounds):
+        self.bounds = new_bounds
+
+    def addObstacle(self, new_obstacle):
+        self.obstacle_pts.append(new_obstacle)
 
 
-def inner_ellipsoid():
+def inflate_region(problem, options, debug=None):
     pass
 
-def inflate_region():
+
+def separating_hyperplanes(obstacle_pts, ellipsoid, polyhedron, infeasible_start):
     pass
+
+# in iris_cdd.h
+def getGenerators(A, b, points, rays):
+    pass
+
+"""
+ in iris_mosek.cpp
+"""
+
+def inner_ellipsoid(polyhedron, ellipsoid, existing_env=None):
+    pass
+
+def closest_point_in_convex_hull(points, result, existing_env=None):
+    pass
+
+def check_res(res):
+    pass
+
+def extract_solution():
+    pass
+
+
+
 
